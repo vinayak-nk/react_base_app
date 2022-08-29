d7c64b99eac99a9bdf7a6d57c7411ceff9808c0b
vinayakkurugovinakoppa <vinayakkurugovinakoppa@qnap.com>
2022-08-29 19:30:15 +0530

    Revert "component with a Modal"
    This reverts commit 506f355f560df97d054c6ab0bcc8e06e2b5e6e15.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import React, { useState } from 'react';
-
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
-
 function App() {
-  const [usersList, setUsersList] = useState([]);
-
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
-
   return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
+    <div className="App">
+      BASE APP
     </div>
   );
 }
