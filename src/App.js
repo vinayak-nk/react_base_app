03505b4ed321216560d1ce35685282f9ed559be7
vinayakkurugovinakoppa <vinayakkurugovinakoppa@qnap.com>
2022-09-09 16:24:54 +0530

    useCallback dependency example

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import './App.css';
 
 function App() {
   const [show, toggleShow] = useState(false)
+  const [allowToggle, seToggling] = useState(false)
 
   console.log('APP RUNNING')
   
   // useCallback(() => {}, [])
-  const handleBtnClick = useCallback(() => {toggleShow((prevShow) => !prevShow)}, [])
+  const handleBtnClick = useCallback(() => {
+    if (allowToggle) toggleShow((prevShow) => !prevShow)
+  }, [allowToggle])
+
+  const handleToggling = useCallback(() => { seToggling((prevAllowToggle) => !prevAllowToggle)}, [])
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
-      <Button onClick={handleBtnClick} >Toggle Paragraph</Button>
+      <DemoOutput show={show} />
+      <Button id="1" onClick={handleBtnClick} >Toggle Paragraph</Button>
+      <div>&nbsp;</div>
+      <Button id="2" onClick={handleToggling} >Allow Toggle </Button>
     </div>
   );
 }
