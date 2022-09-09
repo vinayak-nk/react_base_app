5537b975f1a7bb165e183ce4343979111dbb95b7
vinayakkurugovinakoppa <vinayakkurugovinakoppa@qnap.com>
2022-09-09 14:45:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useState} from 'react';
+
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button'
+import './App.css';
+
 function App() {
+  const [show, toggleShow] = useState(false)
+
+  console.log('APP RUNNING')
+  
+  const handleBtnClick = () => {toggleShow((prevShow) => !prevShow)}
+
   return (
-    <div className="App">
-      BASE APP
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={show} />
+      <Button onClick={handleBtnClick} >Toggle Paragraph</Button>
     </div>
   );
 }
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function MyParagraph({ show }) {
+  console.log('MyParagraph RUNNING')
+  return (
+    <div>{show ? 'My Paragraph': ''}</div>
+  )
+}
+
+export default MyParagraph
